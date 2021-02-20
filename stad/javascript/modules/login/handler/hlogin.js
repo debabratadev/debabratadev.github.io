786f83a8f5a15b9e976c86b64a5795923097085c
debabratadev <debabratakandev@gmail.com>
2021-02-20 14:15:20 +0530

    Remove stad.

--- stad/javascript/modules/login/handler/hlogin.js
+++
@@ -1,9 +0,0 @@
-/**
- * Handles event for login
- */
-window.onload = function () {
-    let loginBtn = document.getElementById('login-btn');
-    loginBtn.addEventListener('click', logInModule.validateForm);
-};
-
-
